1d98286ab246494ba514a01a4d5f2a37f056d776
Gordon Mason <gmason0702@gmail.com>
2021-02-11 20:48:47 -0500

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
-import './Navbar.css'
+import "./Navbar.css";
 
 // Function name matches file name
 const Navbar = () => {
-    // return must have one parent element
-    return (
-        <nav>
-        </nav>
-    )
-}
+  // return must have one parent element
+  return (
+    <nav>
+      <p>Nav Item</p>
+    </nav>
+  );
+};
 
 // Makes it available for import
 export default Navbar;
