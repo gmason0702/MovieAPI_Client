d453dd05927f1e5d9743f08a5842cb7676a4a4b7
Gordon Mason <gmason0702@gmail.com>
2021-03-06 15:06:50 -0500

    still fixing envirojs

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
   case "127.0.0.1":
     APIURL = "http://localhost:3000";
     break;
-  case "https://dlgm-movieradar.herokuapp.com/":
+  case "https://movieradar-app.herokuapp.com/":
     APIURL = "https://dlgm-movieradar.herokuapp.com/";
 }
 
